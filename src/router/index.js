d7c3c29e61405d91192b2ac343e0ba44e74d41ed
Summer-lwc <836651064@qq.com>
2019-12-08 20:49:23 +0800

    完成主页

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,19 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import login from '../components/login.vue';
 import home from '../components/home.vue';
+import welocme from '../components/welcome.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: login },
-  { path: '/home', component: home },
+  { 
+    path: '/home',
+    component: home,
+    redirect: '/welcome',
+    children: [{path: '/welcome', component: welocme}]
+  },
   
 ];
 
